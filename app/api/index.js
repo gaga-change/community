109e6aa08a511f46e336d3c6083bc9dc9e9f5003
严俊东 <1172482914@qq.com>
2018-12-12 20:11:43 +0800

    使用模板、登录注册界面

--- app/api/index.js
+++ app/api/index.js
@@ -2,5 +2,7 @@ const router = require('koa-router')()
 const pages = require('./pages')
 
 router.get('/', pages.home)
+router.get('/user/reg', pages.userReg)
+router.get('/user/login', pages.userLogin)
 
 module.exports = router.routes()
--- app/api/pages.js
+++ app/api/pages.js
@@ -3,5 +3,13 @@ module.exports = {
     /** 主页(列表页) */
     async home(ctx, next) {
         await ctx.render('index', {})
+    },
+    /** 登入 */
+    async userLogin(ctx, next) {
+        await ctx.render('user/login', {})
+    },
+     /** 注册 */
+     async userReg(ctx, next) {
+        await ctx.render('user/reg', {})
     }
 }
