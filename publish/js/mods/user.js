ed53f1854b6efe6dff5b7e7a56690118e238f0cb
严俊东 <1172482914@qq.com>
2019-03-05 16:49:17 +0800

    暂时隐藏第三方登录

--- publish/js/mods/user.js
+++ publish/js/mods/user.js
@@ -158,9 +158,9 @@ layui.define(['laypage', 'fly', 'element', 'flow'], function (exports) {
 
   //根据ip获取城市
   if ($('#L_city').val() === '') {
-    $.getScript('http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js', function () {
-      $('#L_city').val(remote_ip_info.city || '');
-    });
+    // $.getScript('http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=js', function () {
+    //   $('#L_city').val(remote_ip_info.city || '');
+    // });
   }
 
   //上传图片
